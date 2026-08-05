58df6e2ec7629dd2f05823561cc6bff740480939
Devansh Pathak <er.devanshpathak@gmail.com>
2026-08-05 16:37:35 +0530

    Week 2 Day 5: first kernel module - module_init, module_exit, printk (hello.ko built, loaded, unloaded, verified via dmesg)

---
+++ week2/hello_module/.hello.ko.cmd
@@ -0,0 +1,1 @@
+savedcmd_hello.ko := aarch64-linux-gnu-ld -r -EL  -maarch64elf -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T /usr/src/linux-headers-6.18.39+rpt-rpi-2712/arch/arm64/module.lds -o hello.ko hello.o hello.mod.o .module-common.o
